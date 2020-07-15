1a46c9babafde46fbc639494b179051d69b0c3d8
Zheoni <zheoni@outlook.es>
2020-07-15 04:46:34 +0200

    Join JS files of form and fix edit form Sortable bug
    Now the 2 files are just 1 because 2 files where unnecesary.
    
    Also fixed a bug that did not allowed to sort the ingredients list because the Sortable object was being created disabled always.

--- public/javascripts/recipe_form.js
+++ public/javascripts/recipe_form.js
@@ -1,3 +1,29 @@
+const list = document.getElementById("ingredients");
+const template = list.children[list.childElementCount - 1].cloneNode(true);
+
+let sortable = new Sortable(list, {
+  handle: ".handle",
+  animation: 200,
+  touchStartThreshold: 3,
+  disabled: list.childElementCount <= 1
+});
+
+function addIngredient() {
+  const copy = template.cloneNode(true);
+  list.appendChild(copy);
+  
+  sortable.option("disabled", false);
+}
+
+function deleteThisIngredient(ingredient) {
+  if (list.childElementCount > 1) {
+    const container = ingredient.parentElement;
+    container.remove();
+
+    sortable.option("disabled", list.childElementCount <= 1);
+  }
+}
+
 function isEmpty(element) {
   let fields;
   if (["INPUT", "TEXTAREA", "SELECT"].includes(element.nodeName)) {
