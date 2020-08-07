ad485db46e61553462c7920cdc26fde35261a906
Zheoni <zheoni@outlook.es>
2020-08-07 15:09:31 +0200

    Add locale helper program
    A little program that makes adding localised strings easier.

---
+++ localeHelper.js
@@ -0,0 +1,42 @@
+const { getLocale } = require("./localeLoader.js");
+const fs = require("fs");
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const locales = fs.readdirSync("./locales")
+  .map(loc => loc.replace(".json", ""));
+
+function input(question) {
+  return new Promise((resolve) => {
+    rl.question(question, resolve);
+  })
+}
+
+async function promptNewString() {
+  const stringName = await input("String name? ");
+  const stringPath = stringName.split(".");
+
+  console.log("Input now the translations: ");
+  for (const locale of locales) {
+    const translation = await input(locale + "? ");
+
+    const localeObj = getLocale(locale);
+    let s = localeObj;
+    for (let i = 0; i < stringPath.length - 1; ++i) {
+      const key = stringPath[i];
+      if (!(key in s)) {
+        s[key] = {};
+      }
+      s = s[key];
+    }
+    s[stringPath[stringPath.length - 1]] = translation;
+
+    const json = JSON.stringify(localeObj, null, "\t") + "\n";
+    fs.writeFileSync("./locales/" + locale + ".json", json);
+  }
+  process.exit(0);
+}
+
+promptNewString();
